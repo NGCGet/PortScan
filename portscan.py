a2980a0856958d12966f43730ed80f7fa5e71a54
Carlos Eduardo <116766042+NGCGet@users.noreply.github.com>
2026-08-08 01:41:42 -0300

    Refactor portscan.py for improved error handling
    Updated port scanning logic to use context manager for socket. Improved error handling for host resolution and port checking.

--- portscan.py
+++ portscan.py
@@ -3,25 +3,30 @@ import sys
 
 
 def scan(host, portas):
-    try:
-        for porta in portas:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.settimeout(0.05)
-            code = client.connect_ex((host, int(porta)))
+    for porta in portas:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+                client.settimeout(0.5)
+                code = client.connect_ex((host, int(porta)))
 
-            if code == 0:
-                print("{} - open".format(porta))
-    except Exception as error:
-        print(error)
+                if code == 0:
+                    print(f"Porta {porta} - aberta")
+        except socket.gaierror:
+            print(f"Erro: Não foi possível resolver o host '{host}'.")
+            break
+        except Exception as error:
+            print(f"Erro ao verificar porta {porta}: {error}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         host = sys.argv[1]
+
         if len(sys.argv) >= 3:
             portas = sys.argv[2].split(",")
         else:
-            portas = [21, 22, 23, 24, 25, 80, 135, 139, 433, 445, 8080, 8433, 3306]
+            portas = [21, 22, 23, 25, 80, 135, 139, 443, 445, 3306, 8080, 8443]
 
         scan(host, portas)
     else:
-        print("usage: python portscan.py google.com 22,23,80,443")
+        print("Uso correto: python portscan.py google.com 22,23,80,443")
